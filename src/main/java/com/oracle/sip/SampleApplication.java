8ef54544ebe3a906b62b749350afc457ff1de190
wkorando <wkorando@gmail.com>
2023-04-20 10:35:45 -0500

    Initial import

---
+++ src/main/java/com/oracle/sip/SampleApplication.java
@@ -0,0 +1,24 @@
+package com.oracle.sip;
+
+/***
+ * Some class level javadoc
+ * 
+ * <h2 id="my-anchor">My own header</h2>
+ * 
+ * Some important info here. 
+ * @author bkorando
+ *
+ */
+public class SampleApplication {
+	
+	/***
+	 * Some javadoc
+	 *
+	 * 
+	 * @see com.oracle.sip.SampleApplication##my-anchor
+	 * @param args some args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/main/java/com/oracle/sip/package-info.java
@@ -0,0 +1,17 @@
+/**
+* This is some package level info.
+* 
+* <h2>Main header</h2>
+* 
+* Important high-level info here.
+* 
+* <h3>Sub-header</h3>
+* 
+* Other info.
+* 
+* <h4>Minor header</h4>
+* 
+* Less important info.
+**/
+
+package com.oracle.sip;
