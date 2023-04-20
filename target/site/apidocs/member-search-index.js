894c9b4f5d6adfc805095ab435ff76505de86da9
wkorando <wkorando@gmail.com>
2023-04-20 10:46:31 -0500

    Removing unnecessary files

--- target/site/apidocs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.oracle.sip","c":"SampleApplication","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"com.oracle.sip","c":"SampleApplication","l":"SampleApplication()","u":"%3Cinit%3E()"}];updateSearchResults();
